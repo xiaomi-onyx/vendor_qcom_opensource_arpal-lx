2e544e541204e35480e199ebb2109b3f021a9b65
Kishan Kumar Majeti <quic_kkmajeti@quicinc.com>
2023-05-29 10:21:51 +0530

    pal: Avoid pushing offload command if already waiting
    Buffer ready event wait will not be pushed if already waiting
    
    Change-Id: Ifa9637826bec7cb337a4d0d66d47c812ba953c3c

--- session/src/SessionAlsaCompress.cpp
+++ session/src/SessionAlsaCompress.cpp
@@ -700,6 +700,9 @@ void SessionAlsaCompress::offloadThreadLoop(SessionAlsaCompress* compressObj)
             compressObj->msg_queue_.pop();
             lock.unlock();
 
+            if (msg && msg->cmd)
+                compressObj->command = msg->cmd;
+
             if (msg && msg->cmd == OFFLOAD_CMD_EXIT)
                 break; // exit the thread
 
@@ -709,6 +712,7 @@ void SessionAlsaCompress::offloadThreadLoop(SessionAlsaCompress* compressObj)
                     ret = compress_wait(compressObj->compress, -1);
                     PAL_VERBOSE(LOG_TAG, "out of compress_wait, ret %d", ret);
                     event_id = PAL_STREAM_CBK_EVENT_WRITE_READY;
+                    compressObj->command = OFFLOAD_CMD_EXIT;
                 }
             } else if (msg && msg->cmd == OFFLOAD_CMD_DRAIN) {
                 if (!is_drain_called) {
@@ -2043,11 +2047,15 @@ int SessionAlsaCompress::write(Stream *s __unused, int tag __unused, struct pal_
 
     if (bytes_written >= 0 && bytes_written < (ssize_t)buf->size && non_blocking) {
         PAL_DBG(LOG_TAG, "No space available in compress driver, post msg to cb thread");
-        std::shared_ptr<offload_msg> msg = std::make_shared<offload_msg>(OFFLOAD_CMD_WAIT_FOR_BUFFER);
-        std::lock_guard<std::mutex> lock(cv_mutex_);
-        msg_queue_.push(msg);
 
-        cv_.notify_all();
+        if (command != OFFLOAD_CMD_WAIT_FOR_BUFFER)
+        {
+            std::shared_ptr<offload_msg> msg = std::make_shared<offload_msg>(OFFLOAD_CMD_WAIT_FOR_BUFFER);
+            std::lock_guard<std::mutex> lock(cv_mutex_);
+            msg_queue_.push(msg);
+
+            cv_.notify_all();
+        }
     }
 
     if (!playback_started && bytes_written > 0) {
