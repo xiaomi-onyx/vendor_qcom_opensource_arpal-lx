e6581e9c4a6df9fc6afbe1460f48c7759be4bc59
Lakshmi Prahitha Pendam <quic_pendam@quicinc.com>
2024-06-19 00:33:05 +0530

    pal: follow up of pal callback change
    Add device ID and stream type filter for callback
    
    Change-Id: I27d5ea8150f5e3645c16f7ceb6ee41903469f96e

--- ipc/aidl/palnotifierserver/PalServerNotify.h
+++ ipc/aidl/palnotifierserver/PalServerNotify.h
@@ -8,11 +8,11 @@
 #include <aidl/vendor/qti/hardware/paleventnotifier/IPALEventNotifierCallback.h>
 #include <aidl/vendor/qti/hardware/paleventnotifier/IPALEventNotifier.h>
 #include <log/log.h>
+#include <cutils/list.h>
 #include "PalApi.h"
-
+#include "PalDefs.h"
 
 using PalCallbackConfig = ::aidl::vendor::qti::hardware::paleventnotifier::PalCallbackConfig;
-using PalStreamType = ::aidl::vendor::qti::hardware::paleventnotifier::PalStreamType;
 
 namespace aidl::vendor::qti::hardware::paleventnotifier {
 
@@ -21,6 +21,27 @@ class PalServerNotify : public BnPALEventNotifier {
   public:
     virtual ~PalServerNotify() {}
     ::ndk::ScopedAStatus ipc_pal_notify_register_callback(const std::shared_ptr<IPALEventNotifierCallback>& cb, int* ret) override;
+    ::ndk::ScopedAStatus ipc_pal_notify_register_callback_v2(const std::shared_ptr<IPALEventNotifierCallback>& cb,
+        const std::vector<PalDeviceId>& devID, const std::vector<PalStreamType>& streamType, int* ret);
+};
+
+class ClientInfo {
+public:
+    struct listnode list;
+    uint32_t pid;
+    std::shared_ptr<IPALEventNotifierCallback> pal_clbk;
+    bool isnotified;
+    std::vector<PalDeviceId> deviceID;
+    std::vector<PalStreamType> streamType;
+    ::ndk::ScopedAIBinder_DeathRecipient mDeathRecipient;
+    int getPid() { return pid; }
+    ClientInfo(int pid) {
+        mDeathRecipient = ndk::ScopedAIBinder_DeathRecipient(
+                AIBinder_DeathRecipient_new(ClientInfo::onDeath));
+    }
+    static void onDeath(void *cookie);
+    void onDeath();
+    void linkToDeath(const std::shared_ptr<IPALEventNotifierCallback>& cb, ClientInfo *client_handle);
 };
 
 }
