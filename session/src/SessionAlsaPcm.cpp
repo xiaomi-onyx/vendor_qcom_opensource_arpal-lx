8f5fc283253d5902b3f49395c7c8cbb396ca8ca2
Saurav Kumar <quic_sauravk@quicinc.com>
2024-07-01 19:39:59 +0530

    pal: Add fix to skip backned device check for incall record/playback
    Add changes to skip check for backend devices for incall record
    and incall playback usecases.
    
    Change-Id: I798d9f0172f5ccfb1a7f657a15014277ab9e013e

--- session/src/SessionAlsaPcm.cpp
+++ session/src/SessionAlsaPcm.cpp
@@ -238,10 +238,6 @@ int SessionAlsaPcm::open(Stream * s)
             if (sAttr.type == PAL_STREAM_ACD || sAttr.type == PAL_STREAM_SENSOR_PCM_DATA)
                 ldir = TX_HOSTLESS;
 
-            if (txAifBackEnds.empty()) {
-                PAL_ERR(LOG_TAG, "no TX backend specified for this stream\n");
-                return -EINVAL;
-            }
             pcmDevIds = rm->allocateFrontEndIds(sAttr, ldir);
             if (pcmDevIds.size() == 0) {
                 PAL_ERR(LOG_TAG, "allocateFrontEndIds failed");
@@ -256,10 +252,6 @@ int SessionAlsaPcm::open(Stream * s)
             }
             if (sAttr.type == PAL_STREAM_SENSOR_PCM_RENDERER) {
                 ldir = RX_HOSTLESS;
-            }
-            if (rxAifBackEnds.empty()) {
-                PAL_ERR(LOG_TAG, "no RX backend specified for this stream\n");
-                return -EINVAL;
             }
             pcmDevIds = rm->allocateFrontEndIds(sAttr, ldir);
             if (pcmDevIds.size() == 0) {
