CodeLinaro-mirror/la_platform_vendor_qcom_opensource_qal

pal: Add error handling if allocate frontend fails
Free frontend if one of the frontend allocation fails in
loopback usecase

Change-Id: I1a08dc16e04f49180e7aace6ae4a878a7d0c8c02

session/src/SessionAlsaPcm.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2019-2020, The Linux Foundation. All rights reserved.
  *
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -171,6 +171,14 @@ int SessionAlsaPcm::open(Stream * s)
             pcmDevRxIds = rm->allocateFrontEndIds(sAttr, RXLOOPBACK);
             pcmDevTxIds = rm->allocateFrontEndIds(sAttr, TXLOOPBACK);
             if (!pcmDevRxIds.size() || !pcmDevTxIds.size()) {
+                if (pcmDevRxIds.size()) {
+                    PAL_ERR(LOG_TAG, "freeFrontEndIds Rx called as failed");
+                    rm->freeFrontEndIds(pcmDevRxIds, sAttr, RXLOOPBACK);
+                }
+                if (pcmDevTxIds.size()) {
+                    PAL_ERR(LOG_TAG, "freeFrontEndIds Tx called as failed");
+                    rm->freeFrontEndIds(pcmDevTxIds, sAttr, TXLOOPBACK);
+                }
                 PAL_ERR(LOG_TAG, "allocateFrontEndIds failed");
                 return -EINVAL;
             }

xiaomi-onyx/vendor_qcom_opensource_arpal-lx

session/src/SessionAlsaPcm.cpp
@@ -300,6 +300,14 @@ int SessionAlsaPcm::open(Stream * s)
             pcmDevRxIds = rm->allocateFrontEndIds(sAttr, RX_HOSTLESS);
             pcmDevTxIds = rm->allocateFrontEndIds(sAttr, TX_HOSTLESS);
             if (!pcmDevRxIds.size() || !pcmDevTxIds.size()) {
+                if (pcmDevRxIds.size()) {
+                    PAL_ERR(LOG_TAG, "freeFrontEndIds Rx called as failed");
+                    rm->freeFrontEndIds(pcmDevRxIds, sAttr, RX_HOSTLESS);
+                }
+                if (pcmDevTxIds.size()) {
+                    PAL_ERR(LOG_TAG, "freeFrontEndIds Tx called as failed");
+                    rm->freeFrontEndIds(pcmDevTxIds, sAttr, TX_HOSTLESS);
+                }
                 PAL_ERR(LOG_TAG, "allocateFrontEndIds failed");
                 status = -EINVAL;
                 goto exit;