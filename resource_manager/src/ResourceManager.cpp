5f0a41230beb3fd6d13fd8f437defa8bb203b5a7
Zhou Song <quic_zhous@quicinc.com>
2025-03-28 18:54:34 +0800

    ar-pal: avoid duplicated mutex unlock when connecting streams
    During connecting streams, connect stream list may already have
    duplicated streams with different device ids, this may lead
    duplicated unlock of stream mutex.
    Check if stream already unlocks mutex to avoid this.
    
    Change-Id: I0b37822ddc6f2e8ceee5d1fd8da2a8673b0b7cd0

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -8396,6 +8396,7 @@ error:
 int32_t ResourceManager::streamDevConnect_l(std::vector <std::tuple<Stream *, struct pal_device *>> streamDevConnectList){
     int status = 0;
     std::vector <std::tuple<Stream *, struct pal_device *>>::iterator sIter;
+    std::set<Stream *> connected_streams;
 
     PAL_DBG(LOG_TAG, "Enter");
     /* connect active list from the current devices they are attached to */
@@ -8409,7 +8410,9 @@ int32_t ResourceManager::streamDevConnect_l(std::vector <std::tuple<Stream *, st
                 PAL_DBG(LOG_TAG,"connected stream %pK from device %d",
                         std::get<0>(*sIter), (std::get<1>(*sIter))->id);
             }
-            std::get<0>(*sIter)->unlockStreamMutex();
+            auto result = connected_streams.insert(std::get<0>(*sIter));
+            if (result.second)
+                std::get<0>(*sIter)->unlockStreamMutex();
         }
     }
 
